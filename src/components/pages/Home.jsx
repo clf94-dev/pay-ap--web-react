129c73a891fb41f3e3ac59475fcc91b2dbf528d3
clf94-dev <clf94@hotmail.es>
2020-09-26 18:54:41 +0200

    added companies component to home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Companies from '../Companies'
 import TopHome from '../TopHome'
 
 function Home() {
     return (
         <div>
             <TopHome />
+            <Companies/>
         </div>
     )
 }
