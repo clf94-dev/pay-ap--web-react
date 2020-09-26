52fcd896ad7e1838e8b99c47bf0af3553fea8823
clf94-dev <clf94@hotmail.es>
2020-09-26 17:40:24 +0200

    added routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,27 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Pricing from './pages/Pricing'
+
+function App() {
+    return (
+        <div>
+            <Router>
+
+                <Switch>
+
+                    <Route path='/' exact component={Home}></Route>
+                    <Route path='/pricing' component={Pricing}></Route>
+                    <Route path='/about' component={About}></Route>#
+                    <Route path='/contact' component={Contact}></Route>
+
+                </Switch>
+            </Router>
+        </div>
+    )
+}
+
+export default App
