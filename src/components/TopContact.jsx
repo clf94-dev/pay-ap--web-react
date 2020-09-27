1fb84f9ca710418b0387f38dcdf47c5d11622047
clf94-dev <clf94@hotmail.es>
2020-09-27 08:40:13 +0200

    top title contact page added

---
+++ src/components/TopContact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopContact() {
+    return (
+        <div>
+            <h1>Submit a help request and we’ll get in touch shortly.</h1>
+        </div>
+    )
+}
+
+export default TopContact
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import TopContact from '../TopContact'
 
 function Contact() {
     return (
         <div>
-            
+            <TopContact/>
         </div>
     )
 }
