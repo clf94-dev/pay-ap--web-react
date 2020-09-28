c8fdddbdbd4d9309539b53049200ab2b3962fdcc
clf94-dev <clf94@hotmail.es>
2020-09-28 13:03:20 +0200

    top home component style added

--- src/components/TopHome.jsx
+++ src/components/TopHome.jsx
@@ -3,24 +3,26 @@ import React from 'react'
 
 import PhoneD from './images/assets/home/desktop/illustration-phone-mockup.svg'
 
+import './styles/App.css'
+
 function TopHome() {
     return (
-        <Grid container direction='row'>
-            <Grid item lg={6} md={12}>
+        <Grid container direction='row' className='top-home-cont'>
+            <Grid item lg={6} md={12} className='text-col'>
                 <h1>Start building with our APIs for absolutely free.</h1>
 
-                <Grid container direction='row'>
+                <Grid container direction='row' className='input-row'>
                     <Grid item md={6} xs={12}>
-                        <input type="text"/>
+                        <input placeholder='Enter email address...'type="text"/>
                     </Grid>
                     <Grid item md={6} xs={12}>
-                        <button>Schedule A Demo</button>
+                        <button className='pink-btn'>Schedule A Demo</button>
                     </Grid>
 
                 </Grid>
 
             </Grid>
-            <Grid item lg={6} md={12}>
+            <Grid item lg={6} md={12} className='img-col'>
                 <img src={PhoneD} alt="phone notification"/>
             </Grid>
         </Grid>
