5f2354b64fbedae7ed071c20ac676f12ead830df
clf94-dev <clf94@hotmail.es>
2020-09-29 13:12:49 +0200

    added links to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import {Grid} from '@material-ui/core'
 import React from 'react'
 import LogoW from './images/assets/shared/desktop/logo copy.svg'
+import {Link} from 'react-router-dom'
 
 import './styles/App.css'
 
@@ -8,16 +9,16 @@ function Footer() {
     return (
         <Grid container direction='row' className='footer-cont'>
             <Grid item md={2} xs={12}>
-                <img src={LogoW} alt="PayApi"/>
+              <Link to='/' >  <img src={LogoW} alt="PayApi"/> </Link>
             </Grid>
             <Grid item md={2} xs={12}>
-                <p>Pricing</p>
+               <Link to='/pricing'> <p>Pricing</p></Link>
             </Grid>
             <Grid item md={2} xs={12}>
-                <p>About</p>
+               <Link to='/about'> <p>About</p></Link>
             </Grid>
             <Grid item md={2} xs={12}>
-                <p>Contact</p>
+               <Link to='/contact'> <p>Contact</p></Link>
             </Grid>
             <Grid item md={2} xs={12}></Grid>
             <Grid item md={2} xs={12}>
