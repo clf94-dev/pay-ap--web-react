4883d3d1f576c3d542f5b5c32caab24a464965d5
clf94-dev <clf94@hotmail.es>
2020-09-29 14:19:17 +0200

    added style TopAbout component

--- src/components/TopAbout.jsx
+++ src/components/TopAbout.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import './styles/App.css'
+
 function TopAbout() {
     return (
-        <div>
+        <div className='top-about-cont'>
             <h1>We empower innovators by delivering access to the financial system</h1>
         </div>
     )
