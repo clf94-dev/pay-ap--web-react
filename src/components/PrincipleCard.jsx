bf8673ceaac4d8e165729a5b601920d56e5e3264
clf94-dev <clf94@hotmail.es>
2020-09-29 14:27:55 +0200

    added style to principles component

--- src/components/PrincipleCard.jsx
+++ src/components/PrincipleCard.jsx
@@ -1,15 +1,16 @@
-import { Grid } from '@material-ui/core'
+import {Grid} from '@material-ui/core'
 import React from 'react'
+import './styles/App.css'
 
 function PrincipleCard(props) {
     return (
-        <Grid container direction='row'>
-          <Grid item sm={4} xs={12}  >
-    <h4>{props.title}</h4>
-          </Grid>
-          <Grid item sm={8} xs={12}  >
-    <p>{props.text}</p>
-              </Grid>
+        <Grid container direction='row' className='principles-cont'>
+            <Grid item sm={4} xs={12}>
+                <h4>{props.title}</h4>
+            </Grid>
+            <Grid item sm={8} xs={12}>
+                <p>{props.text}</p>
+            </Grid>
         </Grid>
     )
 }
