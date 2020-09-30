c74099c70a14321608a410f41987189e6e3804bf
clf94-dev <clf94@hotmail.es>
2020-09-30 18:43:45 +0200

    added style to companies section in Contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -10,36 +10,38 @@ import OracleD from '../images/assets/shared/desktop/oracle.svg'
 import GoogleD from '../images/assets/shared/desktop/google.svg'
 import NvidiaD from '../images/assets/shared/desktop/nvidia.svg'
 
+import '../styles/App.css'
+
 function Contact() {
 
    
     return (
         <div>
             <TopContact/>
-            <Grid container direction='row'>
+            <Grid container direction='row' className='contact-cont'>
                 <Grid item lg={6} md={12}>
                     <Form/>
                 </Grid>
-                <Grid item lg={6} md={12}>
+                <Grid item lg={6} md={12} className='contact-comp-cont'>
                     <h3>Join the thousands of innovators already building with us</h3>
-                    <Grid container direction='row'>
+                    <Grid container direction='row' className='contact-comp-row'>
                         <Grid item sm={4} xs={6}>
-                            <img src={TeslaD} alt="Tesla"/>
+                            <img src={TeslaD}className='company-logo' alt="Tesla"/>
                         </Grid>
                         <Grid item sm={4} xs={6}>
-                            <img src={MicrosoftD} alt="Microsoft"/>
+                            <img src={MicrosoftD}className='company-logo' alt="Microsoft"/>
                         </Grid>
                         <Grid item sm={4} xs={6}>
-                            <img src={PackardD} alt="Hewlett Packard"/>
+                            <img src={PackardD}className='company-logo' alt="Hewlett Packard"/>
                         </Grid>
                         <Grid item sm={4} xs={6}>
-                            <img src={OracleD} alt="Oracle"/>
+                            <img src={OracleD}className='company-logo' alt="Oracle"/>
                         </Grid>
                         <Grid item sm={4} xs={6}>
-                            <img src={GoogleD} alt="Google"/>
+                            <img src={GoogleD}className='company-logo' alt="Google"/>
                         </Grid>
                         <Grid item sm={4} xs={6}>
-                            <img src={NvidiaD} alt="NVidia"/>
+                            <img src={NvidiaD}className='company-logo' alt="NVidia"/>
                         </Grid>
                     </Grid>
                 </Grid>
