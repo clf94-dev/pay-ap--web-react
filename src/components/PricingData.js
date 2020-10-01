1afb32297498f7e1905feaf80756e6fdef879493
clf94-dev <clf94@hotmail.es>
2020-10-01 09:21:28 +0200

    added pricing data

--- src/components/PricingData.js
+++ src/components/PricingData.js
@@ -1,6 +1,44 @@
 export const PricingData = [
 
     {
+        index: 0,
+        title: 'Free Plan',
+        text: 'Build and test using our core set of products with up to 100 API requests ',
+        price: '$0.00',
+        transTick: true,
+        authTick: true,
+        identTick: true,
+        investTick: false,
+        assetsTick: false,
+        liabilitTick: false,
+        incoTick: false
+
+    }, {
+        index: 1,
+        title: 'Basic Plan',
+        text: 'Launch your project with unlimited requests and no contractual minimums ',
+        price: '$249.00',
+        transTick: true,
+        authTick: true,
+        identTick: true,
+        investTick: true,
+        assetsTick: true,
+        liabilitTick: false,
+        incoTick: false
+
+    }, {
+        index: 2,
+        title: 'Premium Plan',
+        text: 'Get tailored solutions, volume pricing, and dedicated support for your team ',
+        price: '$499.00',
+        transTick: true,
+        authTick: true,
+        identTick: true,
+        investTick: true,
+        assetsTick: true,
+        liabilitTick: true,
+        incoTick: true
 
     }
+
 ]
