8dc35e179a7b088e924b6ee6d0e131a215b1b4c6
clf94-dev <clf94@hotmail.es>
2020-10-01 09:38:59 +0200

    added PricingData to PricingCard ( pricing struct)

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -1,68 +1,71 @@
-import { Grid } from '@material-ui/core'
+import {Grid} from '@material-ui/core'
 import React from 'react'
 
 function PricingCard(props) {
-    return(
-        <Grid container >
-            <h3></h3>
-            <p></p>
-            <h1></h1>
+    return (
+        <Grid item md={4} xs={12}>
+            <h3>{props.title}
+            </h3>
+            <p>{props.text}
+            </p>
+            <h1>{props.price}
+            </h1>
             <hr/>
             <Grid container direction='row'>
                 <Grid item xs={2}>
-
+                    <i className={props.checkTrans && 'fas fa-check'}></i>
                 </Grid>
                 <Grid item xs={10}>
                     <h4>Transactions</h4>
-                    </Grid>
+                </Grid>
             </Grid>
             <Grid container direction='row'>
                 <Grid item xs={2}>
-
+                    <i className={props.checkAuth && 'fas fa-check'}></i>
                 </Grid>
                 <Grid item xs={10}>
-                <h4>Auth</h4>
-                    </Grid>
+                    <h4>Auth</h4>
+                </Grid>
             </Grid>
             <Grid container direction='row'>
                 <Grid item xs={2}>
-
+                    <i className={props.checkIdent && 'fas fa-check'}></i>
                 </Grid>
                 <Grid item xs={10}>
-                <h4>Identity</h4>
-                    </Grid>
+                    <h4>Identity</h4>
+                </Grid>
             </Grid>
             <Grid container direction='row'>
                 <Grid item xs={2}>
-
+                    <i className={props.checkInvest && 'fas fa-check'}></i>
                 </Grid>
                 <Grid item xs={10}>
-                <h4>Investments</h4>
-                    </Grid>
+                    <h4>Investments</h4>
+                </Grid>
             </Grid>
             <Grid container direction='row'>
                 <Grid item xs={2}>
-
+                    <i className={props.checkAssets && 'fas fa-check'}></i>
                 </Grid>
                 <Grid item xs={10}>
-                <h4>Assets</h4>
-                    </Grid>
+                    <h4>Assets</h4>
+                </Grid>
             </Grid>
             <Grid container direction='row'>
                 <Grid item xs={2}>
-
+                    <i className={props.checkLiabil && 'fas fa-check'}></i>
                 </Grid>
                 <Grid item xs={10}>
-                <h4>Liabilities</h4>
-                    </Grid>
+                    <h4>Liabilities</h4>
+                </Grid>
             </Grid>
             <Grid container direction='row'>
                 <Grid item xs={2}>
-
+                    <i className={props.checkInc && 'fas fa-check'}></i>
                 </Grid>
                 <Grid item xs={10}>
-                <h4>Income</h4>
-                    </Grid>
+                    <h4>Income</h4>
+                </Grid>
             </Grid>
             <hr/>
             <button className='dark-btn'>Request Access</button>
--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -1,10 +1,29 @@
+import {Grid} from '@material-ui/core'
 import React from 'react'
+import PricingCard from '../PricingCard'
+import {PricingData} from '../PricingData'
 import TopPricing from '../TopPricing'
 
 function Pricing() {
     return (
         <div>
             <TopPricing/>
+            <Grid container direction='row'>
+                {PricingData.map(data => {
+                    return (<PricingCard
+                        key={data.index}
+                        title={data.title}
+                        text={data.text}
+                        price={data.price}
+                        checkTrans={data.transTick}
+                        checkAuth={data.authTick}
+                        checkIdent={data.identTick}
+                        checkInvest={data.investTick}
+                        checkAssets={data.assetsTick}
+                        checkLiabil={data.liabilitTick}
+                        checkInc={data.incoTick}/>)
+                })}
+            </Grid>
         </div>
     )
 }
