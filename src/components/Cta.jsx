46dc7a30ae92e076b7419994fbef5191b0612abb
clf94-dev <clf94@hotmail.es>
2020-10-02 11:46:45 +0200

    added animation cta component

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,14 +1,19 @@
 import {Grid} from '@material-ui/core'
 import React from 'react'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
+AOS.init();
+
 function Cta() {
     return (
         <Grid container direction='row' className='cta-cont' >
             <Grid item md={6} xs={12} className='text-col'>
-                <h2>Ready to start?</h2>
+                <h2 data-aos='fade-right'>Ready to start?</h2>
             </Grid>
             <Grid item md={6} xs={12} className='input-col'>
-                <Grid container direction='row'>
+                <Grid container direction='row' data-aos='fade-left' data-aos-delay='200'>
                     <Grid item sm={8} xs={12}>
                         <input placeholder='Enter email address ...'type="text"/>
                     </Grid>
