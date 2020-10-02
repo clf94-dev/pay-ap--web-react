ccf5ad1b7f4f351ed4be357c7c04760fc24d0d62
clf94-dev <clf94@hotmail.es>
2020-10-02 11:41:27 +0200

    added animation to middle component home page

--- src/components/MiddleHomeCard.jsx
+++ src/components/MiddleHomeCard.jsx
@@ -4,17 +4,22 @@ import React from 'react'
 import TerminalD from './images/assets/home/desktop/illustration-easy-to-implement.svg'
 import SimpleUID from './images/assets/home/desktop/illustration-simple-ui.svg'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
+AOS.init();
+
 function MiddleHomeCard(props) {
     const MiddleImgD = [TerminalD, SimpleUID];
 
     return (
         <Grid container direction='row'  className={props.classes}>
             <Grid item lg={6} md={12} className='text-col'>
-                <h2>{props.title}</h2>
-                <p>{props.text}</p>
+                <h2 data-aos='fade-up' data-aos-delay='200'>{props.title}</h2>
+                <p data-aos='fade-up' data-aos-delay='400'>{props.text}</p>
             </Grid>
             <Grid item lg={6} md={12} className='img-col'>
-                <img src={MiddleImgD[props.index]} alt={props.alt}/>
+                <img data-aos='fade-up' data-aos-delay='200' src={MiddleImgD[props.index]} alt={props.alt}/>
             </Grid>
         </Grid>
 
